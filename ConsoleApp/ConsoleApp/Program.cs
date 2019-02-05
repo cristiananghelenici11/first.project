9019bbfa1da045512865a761f27b630e82719d47
Cristian Anghelenici <cristian.anghelenici@amdaris.com>
2019-02-05 16:31:58 +0200

    change text in master

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello 1!");
             Console.ReadKey();
 
         }
