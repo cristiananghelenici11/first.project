abfc83d67d6f1b9ed2d2585ae51eec29590d1136
Anghelenici Cristian <cristian.anghelenici@ati.utm.md>
2019-02-13 18:04:00 +0200

    p7 start

--- Project_6 Array/ArrayApp/ArrayApp/Program.cs
+++ Project_6 Array/ArrayApp/ArrayApp/Program.cs
@@ -13,41 +13,25 @@ namespace ArrayApp
             //Create two functions with one-dimensional arrays
             // Sort, Show, RandomInitialize, concatArray
 
-            ArrayOperation.Show(ArrayOperation.SortUnidimensional(new int[]{15, 2, 40, 10, -50}));
 
             ArrayOperation.Show(ArrayOperation.concatArray(new int[] {10, -2, 100}, ArrayOperation.RandomInitialize(new int[10])));
 
             //Create two functions with multi-dimensional arrays
-            ArrayOperation.Show(ArrayOperation.SortBidimensional(ArrayOperation.RandomInitialize(new int[5, 5])));
+ 
+            
 
-            // Jagged array
-            // Initialize the elements:
-            int[][] arr = new int[][]
-            {
-                new int[] {1, 3, 5, 7, 9},
-                new int[] {1, 3, 5, 7, 9, 5}
-            };              
-            int[][,] arr2 = new int[][,]
-            {
-                new int[,] {{1, 3, 5, 7, 9}, {3, 4, 1, 7, 4}},
-                new int[,] {{1, 3, 5, 7, 9}, {2, 3, 4, 3, 7}}
-            };
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+                    System.Console.Write("{0}{1}", arr[i][j], j == (arr[i].Length - 1) ? "" : " ");
+                }
+                System.Console.WriteLine();
+            }
 
-            ArrayOperation.Show(arr);
-            ArrayOperation.Show(arr2);
-            Array.Sort(ArrayOperation.RandomInitialize(new int[22]));
-            int n = ArrayOperation.RandomInitialize(new int[10, 3]).Rank;
-            Console.WriteLine(n);
-            Array.Reverse(arr);
+            ////
 
-            Console.WriteLine("String");
-            StringExample.StringGo();
-
-            Console.WriteLine("String Builder");
-            StringExample.StringBuilderGo();
-
-            Console.ReadLine();
-
+            Console.ReadKey();
         }
+
+      
     }
 }
