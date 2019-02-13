a9b0bd16cb71be95ec8bb1ba96c88c0e894fb34b
Anghelenici Cristian <cristian.anghelenici@ati.utm.md>
2019-02-13 18:29:00 +0200

    p7 first

---
+++ Project_7 Overload/Overload/Overload/Angle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overload
+{
+    class Angle
+    {
+        //degrees, minutes, seconds
+
+        private double _degrees;
+        private double _minutes;
+        private double _seconds;
+    }
+}
